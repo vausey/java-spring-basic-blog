ceb01424c2fa595483d5399bba2f7068b8552f65
vausey <53297055+vausey@users.noreply.github.com>
2021-03-01 15:04:31 +0000

    Module 4 complete

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -17,6 +17,12 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+             for (Post post : ALL_POSTS) {
+                 if(post.getId() == id){
+                     return post;
+                 }
+
+             }
         return null;
     }
 
